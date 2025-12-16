10b0954c78d19790f8b4c2f223a9207dd74b6c62
Davi de Souza Farias <169002831+DSFNEW@users.noreply.github.com>
2025-12-16 19:21:03 -0300

    Add files via upload

--- 1.5.3.c
+++ 1.5.3.c
@@ -1,9 +1,22 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
+int AUX_WaitEventTimeout(SDL_Event* evt, Uint32* ms) {
+    Uint32 start = SDL_GetTicks();
+    int ret = SDL_WaitEventTimeout(evt, *ms);
+    Uint32 elapsed = SDL_GetTicks() - start;
+
+    if (elapsed >= *ms) {
+        *ms = 0;
+    } else {
+        *ms -= elapsed;
+    }
+
+    return ret;
+}
+
 int main (int argc, char* args[])
 {
-    /* INICIALIZACAO */
     SDL_Init(SDL_INIT_EVERYTHING);
     SDL_Window* win = SDL_CreateWindow("Animacao Simples",
                          SDL_WINDOWPOS_UNDEFINED,
@@ -12,49 +25,43 @@ int main (int argc, char* args[])
                       );
     SDL_Renderer* ren = SDL_CreateRenderer(win, -1, 0);
 
-    /* EXECUCAO */
     int stop = 0;
     SDL_Rect r = { 0,0, 10,10 };
     SDL_Rect b = { 50, 50, 10, 10};
-    int espera = 500;
+    Uint32 espera = 500; // Mudei para Uint32 para compatibilidade
     int contador = 0;
-    while (stop == 0) {
 
+    while (stop == 0) {
         SDL_Event sair;
         Uint32 antes = SDL_GetTicks();
-
 
         SDL_SetRenderDrawColor(ren, 0xFF,0xFF,0xFF,0x00);
         SDL_RenderClear(ren);
         SDL_SetRenderDrawColor(ren, 0x00,0x00,0xFF,0x00);
         SDL_RenderFillRect(ren, &r);
-        SDL_RenderPresent(ren);
         SDL_SetRenderDrawColor(ren, 0x00,0x00,0x00,0x00);
         SDL_RenderFillRect(ren, &b);
         SDL_RenderPresent(ren);
 
-        int isevt = SDL_WaitEventTimeout(&sair, espera);
+        int isevt = AUX_WaitEventTimeout(&sair, &espera);
+
         if(isevt){
-            espera -= (SDL_GetTicks() - antes);
+
+
             if(sair.type == SDL_KEYDOWN){
                 switch(sair.key.keysym.sym){
-
                     case SDLK_UP:
                         r.y -= 10;
                         break;
-
                     case SDLK_DOWN:
                         r.y += 10;
                         break;
-
                     case SDLK_LEFT:
                         r.x -= 10;
                         break;
-
                     case SDLK_RIGHT:
                         r.x += 10;
                         break;
-
                     default:
                         stop++;
                         break;
@@ -71,11 +78,11 @@ int main (int argc, char* args[])
             //r.x += 10;
             //r.y += 10;
         }
-
     }
+
     /* FINALIZACAO */
     SDL_DestroyRenderer(ren);
     SDL_DestroyWindow(win);
     SDL_Quit();
-
+    return 0;
 }
